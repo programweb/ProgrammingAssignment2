13141234adc12b104038dc1dc3b14d8daa97db1d
zwhale <programweb@gmail.com>
2015-07-24 17:45:13 -0700

    Added R code for matrix, inverse, and cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Getting an inverse matrix can be consuming of system resources. 
+## These functions help minimize that by caching previous results 
+## as needed.
 
-## Write a short comment describing this function
+# example test
+# 1]    m <- matrix(1:4, 2, 2)
+# 2]    t <-makeCacheMatrix()
+# 3]    t$set(m)
+# 4]    cacheSolve(t)
+# 5]    cacheSolve(t)    # see message that it is from cache here
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# makeCacheMatrix has 4 nested functions allowing setting and getting of a matrix or inverse
+makeCacheMatrix <- function( x = matrix() ){
+  im <- NULL
+  set <- function(y){
+    x <<- y
+    im <<- NULL
+  }
+  get <- function() x
+  setIM <- function( imtrx ) im <<- imtrx
+  getIM <- function() im
+  list(set = set, get = get, setIM = setIM, getIM = getIM)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# cacheSolve takes a matrix and returns the inverse (possibly from cache)
+cacheSolve <- function(x, ...){
+  cachedInvX <- x$getIM()
+  if( ! is.null(cachedInvX)){
+    message("getting cached data")
+    return(cachedInvX)
+  }
+  data <- x$get()
+  cachedInvX <- solve(data) %*% data
+  x$setIM( cachedInvX )
+  return( cachedInvX )
 }
